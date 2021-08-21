83a7ea9a96b81e0bfad5713fa03a9e4183d566f6
korra <korrasnipes@gmail.com>
2021-08-21 11:51:05 +0300

    shoulddisplay time

--- oclock.js
+++ oclock.js
@@ -1,6 +1,7 @@
 var http = require("http");
+var dt = require('./myFirstModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('super man');
+    res.end('super man has said the date and time is: ' + dt.myDateTime());
 }).listen(8080);
